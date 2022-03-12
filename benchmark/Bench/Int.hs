3539c53850308745b79fe11cce959a02c6865bdf
Jake Leach <jacobleach@protonmail.com>
2022-03-12 09:45:23 -0600

    add: `Data.Int.Unlifted` benchmarks

--- benchmark/Bench/Int.hs
+++ benchmark/Bench/Int.hs
@@ -19,12 +19,23 @@ benchmark =
     [ bench "maxInt:    slow" $ whnf slow'maxInt (123 :: Int)
     , bench "maxInt:    fast" $ whnf fast'maxInt (123 :: Int)
     , --
+      bench "minInt:    slow" $ whnf slow'minInt (123 :: Int)
+    , bench "minInt:    fast" $ whnf fast'minInt (123 :: Int)
+    , --
       bench "posInt:    slow" $ whnf slow'posInt (123 :: Int)
     , bench "posInt:    fast" $ whnf fast'posInt (123 :: Int)
+    , --
+      bench "negInt:    slow" $ whnf slow'negInt (123 :: Int)
+    , bench "negInt:    fast" $ whnf fast'negInt (123 :: Int)
     , --
       bench "ceilBytes: slow" $ whnf slow'ceilBytesToWord (123 :: Int)
     , bench "ceilBytes: fast" $ whnf fast'ceilBytesToWord (123 :: Int)
     ]
+
+-- -----------------------------------------------------------------------------
+--
+-- 'maxInt'
+--
 
 slow'maxInt :: Int -> Int
 slow'maxInt x = max 100 x
@@ -32,11 +43,43 @@ slow'maxInt x = max 100 x
 fast'maxInt :: Int -> Int
 fast'maxInt (I# x) = I# (maxInt# 100# x)
 
+-- -----------------------------------------------------------------------------
+--
+-- 'minInt'
+--
+
+slow'minInt :: Int -> Int
+slow'minInt x = max -100 x
+
+fast'minInt :: Int -> Int
+fast'minInt (I# x) = I# (minInt# -100# x)
+
+-- -----------------------------------------------------------------------------
+--
+-- 'posInt'
+--
+
 slow'posInt :: Int -> Int
 slow'posInt x = max 0 x
 
 fast'posInt :: Int -> Int
 fast'posInt (I# x) = I# (posInt# x)
+
+-- -----------------------------------------------------------------------------
+--
+-- 'negInt'
+--
+
+slow'negInt :: Int -> Int
+slow'negInt x = max 0 x
+
+fast'negInt :: Int -> Int
+fast'negInt (I# x) = I# (negInt# x)
+
+-- -----------------------------------------------------------------------------
+--
+-- 'ceilBytesToWord'
+--
 
 slow'ceilBytesToWord :: Int -> Int
 slow'ceilBytesToWord x =
